dc27aa5338e265a9b0883d2b26676b77c32f4956
Chevy Lin <chevylin0802@gmail.com>
2023-05-08 10:38:17 +0800

    update

--- resources/views/members/subject.blade.php
+++ resources/views/members/subject.blade.php
@@ -2,4 +2,4 @@
 <div>
   <h2 dir="auto" role="heading" aria-level="1">30年有線電視&nbsp;首次 &nbsp; &nbsp;送大禮，續看+電影追劇&nbsp;&nbsp;75吋</h2>
 </div>
-<h2 dir="auto">活動期至08/30止</h2>
+<h2 dir="auto">活動期至08/31止</h2>
